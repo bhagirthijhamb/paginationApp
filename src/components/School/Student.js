84c5dd6ce6fef0198859bd20c451326f2e30a1c8
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2022-06-27 12:44:22 -0400

    Part 2

--- src/components/School/Student.js
+++ src/components/School/Student.js
@@ -2,20 +2,22 @@ import classes from "./Student.module.css";
 
 const Student = (props) => {
   return (
-    <li>
-      <div>
+    <li className={classes.item}>
+      <div className={classes.studenImage}>
         <img
           src={require("./default-user-image.jpeg")}
           alt="student profile pic"
-          width="60"
-          height="60"
+          // width="60"
+          // height="60"
         />
       </div>
-      <div>
+      <div className={classes.studentDetails}>
         <h3>{props.studentDetails.name}</h3>
-        <p>Email: {props.studentDetails.email}</p>
-        <p>Company: {props.studentDetails.company}</p>
-        <p>Phone: {props.studentDetails.phone}</p>
+        <div className={classes.details}>
+          <p>Email: {props.studentDetails.email}</p>
+          <p>Company: {props.studentDetails.company}</p>
+          <p>Phone: {props.studentDetails.phone}</p>
+        </div>
         {/* <p>Geo: {props.studentDetails.phone}</p> */}
       </div>
     </li>
